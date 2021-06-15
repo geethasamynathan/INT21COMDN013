0bd687c6965985c0e6d211bcce1404c76dd94c7d
geethasamynathan <geethasamynathan2011@gmail.com>
2021-06-15 15:49:07 +0530

    Passing data from Controller to view
    ViewData, ViewBag, Strongly typed view , Http GET, HttpPost, Basic HtmlHelpers

---
+++ EmployeeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoApp1.Models;
+
+namespace DemoApp1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        static List<Employee> employees = new List<Employee>();
+        Employee employee = new Employee();
+        public EmployeeController()
+        {
+            employee.Id = 100;
+            employee.Name = "Akansha";
+            employee.Gender = "Female";
+            Employee employee1 = new Employee();
+            employee1.Id = 101;
+            employee1.Name = "Fransy";
+            employee1.Gender = "Female";
+            //  ViewData["name"] = employee;
+            // ViewBag.empname = employee;
+            employees.Add(employee);
+            employees.Add(employee1);
+
+        }
+      
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+           
+            return View(employees);
+        }
+        [HttpGet]
+        //[NonAction]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            employees.Add(emp);
+            return View("Index",employees);
+        }
+    }
+}
